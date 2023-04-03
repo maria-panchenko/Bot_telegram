d67e861c77ac959bd7897c25b86821f8374cb9f8
mariia_panchenko <124931079+maria-panchenko@users.noreply.github.com>
2023-04-03 11:11:05 +0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import random
 from aiogram import Bot, Dispatcher, types, executor
+from aiogram.dispatcher.filters import Text
 import confidentially as con # достаем токен
 import button as bt # достаем кнопку
 
@@ -14,7 +15,7 @@ async def start_handler(message: types.Message): # обработка декор
     await message.reply(f"Привет, {user_name}, я помогу тебе с мотивацией и пришлю тебе картинку! Напиши мне /picture или нажми кнопку ниже", reply_markup=bt.greet_kb) # высылаем сообщение как ответ
 
 # картинка с кнопки
-@dp.message_handler() # декоратор сообщения с кнопки
+@dp.message_handler(Text(equals="Картинка")) # декоратор сообщения с кнопки
 async def picture_sending(msg: types.Message): # обработка декоратора и приход сообщения
     photo = open('photo/' + random.choice(os.listdir('photo')), 'rb') # достаем рандомную фотку с папки
     await bot.send_photo(msg.from_user.id, photo) # присылаем фото пользвателю
